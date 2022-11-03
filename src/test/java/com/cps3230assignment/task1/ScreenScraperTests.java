df629188e1f3ca25fb164918d697c9290c10e6ab
DanielFarrugiaUM <82520234+DanielFarrugiaUM@users.noreply.github.com>
2022-11-03 11:50:48 +0100

    Initial commit

---
+++ src/test/java/com/cps3230assignment/task1/ScreenScraperTests.java
@@ -0,0 +1,16 @@
+package com.cps3230assignment.task1;
+
+import org.junit.jupiter.api.Test;
+
+public class ScreenScraperTests {
+
+    @Test
+    public void testGetProducts(){
+
+    }
+
+    @Test
+    public void testUploadAlerts(){
+
+    }
+}
